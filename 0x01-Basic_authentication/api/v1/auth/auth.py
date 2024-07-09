614e13e6cbeea4f193bbe722be190284e6087e87
sosmrraziel <lbt_alh@hotmail.fr>
2024-07-09 22:52:17 +0100

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Module of Index views"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Placeholder method for authentication requirement.
+        Returns False for now.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Placeholder method for retrieving authorization header.
+        Returns None for now.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Placeholder method for getting the current user.
+        Returns None for now.
+        """
+        return None
