6d3697b2bd435bc2d02eb048b00223ae9f986a07
sosmrraziel <lbt_alh@hotmail.fr>
2024-07-10 03:00:20 +0100

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 import base64
 from typing import Tuple
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -70,3 +71,30 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(":", 1)
         return email, password
+
+    def user_object_from_credentials(
+                    self, user_email: str, user_pwd: str) -> User:
+        """
+        Retrieves a User instance based on email and password.
+
+        Args:
+            user_email (str): The user's email.
+            user_pwd (str): The user's password.
+
+        Returns:
+            User: The User instance if found, or None if invalid.
+        """
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({"email": user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
