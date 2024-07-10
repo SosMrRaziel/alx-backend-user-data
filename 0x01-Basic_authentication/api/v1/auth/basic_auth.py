c50abd150501799a5dd6f4776ebed9dadeed2cfc
sosmrraziel <lbt_alh@hotmail.fr>
2024-07-10 02:57:00 +0100

    task9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of BasicAuth views"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -47,3 +48,25 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """
+        Extracts the user email and password from the Base64-decoded
+        Authorization header.
+
+        Args:
+            decoded_base64_authorization_header (str): The Base64-decoded
+            Authorization header.
+
+        Returns:
+            Tuple[str, str]: The user email and password, or
+            (None, None) if invalid.
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+                                    decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(":", 1)
+        return email, password
